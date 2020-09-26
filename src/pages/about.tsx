1a7eab1dc9ddc6cd56e8eb800d65265690b4c96a
Muhammad Umar Farooq <umar@orderspot.co>
2020-09-26 13:01:23 +0500

    Component specific styling

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { navigate } from "gatsby"
 import Layout from "../components/Layout"
+import styles from "./about.module.css"
 
 export default function About() {
   return (
     <Layout headerTitle="About Page">
-      <p>About page</p>
+      <p className={styles.myTitle}>About page</p>
       <button onClick={() => {
         navigate("/")
       }}>
