4bd7f90dbe90c103be6bb20e862b1da7230b6606
Muhammad Umar Farooq <umar@orderspot.co>
2020-09-26 08:41:12 +0500

    Sub component in Gatsby

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { navigate } from "gatsby"
+import Header from "../components/Header"
 
 export default function About() {
   return (
     <div>
+      <Header title="About page"/>
       <p>About page</p>
       <button onClick={() => {
         navigate("/")
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Header from "../components/Header"
 
 export default function Home() {
   return (
     <div>
+      <Header title="Index page"/>
       <p>Hello world from index.html!</p>
       <Link to="/about">Go to About Page</Link>
     </div>
