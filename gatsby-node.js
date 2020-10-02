0eeab3d19c28284a6454629a10b8adc321ebe022
Muhammad Umar Farooq <umar@orderspot.co>
2020-10-02 22:04:57 +0500

    create a new page

--- gatsby-node.js
+++ gatsby-node.js
@@ -7,4 +7,13 @@ exports.createPages = async function({ actions }) {
       myName: "Muhammad Umar Farooq"
     }
   })
+  
+  actions.createPage({
+    path: "my-new-dynamic-page",
+    component: require.resolve("./src/templates/dynamic-page.jsx"),
+    context: {
+      myTeacher: "New teacher name",
+      myName: "New student"
+    }
+  })
 }
