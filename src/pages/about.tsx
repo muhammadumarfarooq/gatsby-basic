1eae6d956a9e62deea1649352ed3d5a31dabad4c
Muhammad Umar Farooq <umar@orderspot.co>
2020-10-03 13:42:29 +0500

    Using material ui in gatsby

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -2,16 +2,15 @@ import React from "react"
 import { navigate } from "gatsby"
 import Layout from "../components/Layout"
 import styles from "./about.module.css"
+import Button from "@material-ui/core/Button"
 
 export default function About() {
   return (
     <Layout headerTitle="About Page">
       <p className={styles.myTitle}>About page</p>
-      <button onClick={() => {
-        navigate("/")
-      }}>
-        GO back to the main page
-      </button>
+      <Button onClick={() => navigate("/")} variant="contained" color="primary">
+        Primary
+      </Button>
     </Layout>
   )
 }
